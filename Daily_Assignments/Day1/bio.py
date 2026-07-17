1c02e2453a2ba41c0f675509353773f081eb71e2
ankit jaiswal <ankitjaiswal8726@gmail.com>
2026-07-17 11:23:17 +0530

    About me

---
+++ Daily_Assignments/Day1/bio.py
@@ -0,0 +1,7 @@
+
+print("name","Ankit Jaiswal", sep=" : ")
+print("age","19",sep=" = ")
+print("state","uttar pradesh",sep=" : ", end=",");
+print("city","Azamgarsh", sep="-")
+print("Course","BCA",sep=" - ")
+print("University","University Of Lucknow",sep=" : ")
